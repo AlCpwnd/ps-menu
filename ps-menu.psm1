28fbe1c95a8db6bdf11ee950dde0e22511366147
Alexis De Jaeger <a.dejaeger@hotmail.com>
2025-05-18 13:13:45 +0200

    Removed unused variables. Grouped key codes in a single variable. Replaced multiple if-statements with switches. Cleaned up return part.

--- ps-menu.psm1
+++ ps-menu.psm1
@@ -1,94 +1,92 @@
-function DrawMenu {
+function DrawMenu{
     param ($menuItems, $menuPosition, $Multiselect, $selection)
-    $l = $menuItems.length
-    for ($i = 0; $i -le $l;$i++) {
-		if ($menuItems[$i] -ne $null){
+    for($i = 0; $i -le $menuItems.length;$i++){
+		if($null -ne $menuItems[$i]){
 			$item = $menuItems[$i]
-			if ($Multiselect)
+			if($Multiselect)
 			{
-				if ($selection -contains $i){
+				if($selection -contains $i){
 					$item = '[x] ' + $item
 				}
-				else {
+				else{
 					$item = '[ ] ' + $item
 				}
 			}
-			if ($i -eq $menuPosition) {
+			if($i -eq $menuPosition){
 				Write-Host "> $($item)" -ForegroundColor Green
-			} else {
+			}else{
 				Write-Host "  $($item)"
 			}
 		}
     }
 }
 
-function Toggle-Selection {
-	param ($pos, [array]$selection)
-	if ($selection -contains $pos){ 
-		$result = $selection | where {$_ -ne $pos}
+function Switch-Selection{
+	param($pos,[array]$selection)
+	if($selection -contains $pos){ 
+		$result = $selection | Where-Object{$_ -ne $pos}
 	}
-	else {
+	else{
 		$selection += $pos
 		$result = $selection
 	}
 	$result
 }
 
-function Menu {
-    param ([array]$menuItems, [switch]$ReturnIndex=$false, [switch]$Multiselect)
-    $vkeycode = 0
+function Menu{
+    param([array]$menuItems,[switch]$ReturnIndex=$false,[switch]$Multiselect)
+    $keycode = 0
     $pos = 0
     $selection = @()
-    if ($menuItems.Length -gt 0)
-	{
-		try {
+    if($menuItems.Length -gt 0){
+		try{
+			$keys = [PSCustomObject]@{ # Keycode list: https://learn.microsoft.com/en-us/dotnet/api/system.consolekey?view=net-9.0
+				up = 38,75		# Up arrow, k
+				down = 40,74	# Down arrow, j
+				home = 36		# Home key
+				end = 35		# End key
+				toggle = 32		# Space
+			}
 			$startPos = [System.Console]::CursorTop		
-			[console]::CursorVisible=$false #prevents cursor flickering
+			[System.Console]::CursorVisible = $false #prevents cursor flickering
 			DrawMenu $menuItems $pos $Multiselect $selection
-			While ($vkeycode -ne 13 -and $vkeycode -ne 27) {
-				$press = $host.ui.rawui.readkey("NoEcho,IncludeKeyDown")
-				$vkeycode = $press.virtualkeycode
-				If ($vkeycode -eq 38 -or $press.Character -eq 'k') {$pos--}
-				If ($vkeycode -eq 40 -or $press.Character -eq 'j') {$pos++}
-				If ($vkeycode -eq 36) { $pos = 0 }
-				If ($vkeycode -eq 35) { $pos = $menuItems.length - 1 }
-				If ($press.Character -eq ' ') { $selection = Toggle-Selection $pos $selection }
-				if ($pos -lt 0) {$pos = 0}
-				If ($vkeycode -eq 27) {$pos = $null }
-				if ($pos -ge $menuItems.length) {$pos = $menuItems.length -1}
-				if ($vkeycode -ne 27)
-				{
+			While($keycode -ne 13 -and $keycode -ne 27){
+				$keycode = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown").VirtualKeyCode
+				switch($keycode){
+					{$keys.up -contains $_}{$pos--}
+					{$keys.down -contains $_}{$pos++}
+					{$keys.home -contains $_}{$pos = 0}
+					{$keys.end -contains $_}{$pos = $menuItems.length - 1}
+					{$keys.toggle -contains $_}{$selection = Switch-Selection $pos $selection}
+				}
+				switch($pos){ # When attempting to move the cursor outside of the array
+					{$_ -lt 0}{$pos = 0}
+					{$_ -ge $menuItems.length}{$pos = $menuItems.length -1}
+				}
+				if($keycode -eq 27){ # Esc key
+					$pos = $null
+				}else{
 					$startPos = [System.Console]::CursorTop - $menuItems.Length
 					[System.Console]::SetCursorPosition(0, $startPos)
 					DrawMenu $menuItems $pos $Multiselect $selection
 				}
 			}
-		}
-		finally {
+		}finally{
 			[System.Console]::SetCursorPosition(0, $startPos + $menuItems.Length)
-			[console]::CursorVisible = $true
+			[System.Console]::CursorVisible = $true
 		}
-	}
-	else {
+	}else{
 		$pos = $null
 	}
 
-    if ($ReturnIndex -eq $false -and $pos -ne $null)
-	{
-		if ($Multiselect){
-			return $menuItems[$selection]
+	if($pos){
+		if(-not $Multiselect){
+			$selection = $pos
 		}
-		else {
-			return $menuItems[$pos]
-		}
-	}
-	else 
-	{
-		if ($Multiselect){
+		if($ReturnIndex){
 			return $selection
-		}
-		else {
-			return $pos
+		}else{
+			return $menuItems[$selection]
 		}
 	}
 }
