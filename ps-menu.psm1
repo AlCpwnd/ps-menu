5e3a831996a841c59b123e2f26b2f0f7b3b20899
Alexis De Jaeger <a.dejaeger@hotmail.com>
2025-05-27 22:30:08 +0200

    - Grouped keycode decoding in single switch - Added exceptions if array items aren't measurable - Changed highlighting to match original settings - Added documentation to the "menu" function

--- ps-menu.psm1
+++ ps-menu.psm1
@@ -1,24 +1,24 @@
 function DrawMenu{
     param ($menuItems, $menuPosition, $Multiselect, $selection, $columnCount=$false)
-	$highLight = @{
-		ForegroundColor = [System.Console]::BackgroundColor
-		BackgroundColor = [System.Console]::ForegroundColor
-	}
 	if($columnCount){
-		$width = ($menuItems | Measure-Object -Property Length -Maximum).Maximum
+		$maxWidth = ($menuItems | Measure-Object -Property Length -Maximum).Maximum
 		for($i = 0; $i -le $menuItems.length;$i++){
 			$object = $menuItems[$i]
 			if($object){
-				$text = "$object$(" " * ($width - $object.Length))"
-				if($Multiselect -and $selection -contains $i){
-					$item = "[x] $text "
+				$text = "$object$(" " * ($maxWidth - $object.Length))"
+				if($Multiselect){
+					if($selection -contains $i){
+					$item = "[x] $text"
+					}else{
+						$item = "[ ] $text"
+					}
 				}else{
-					$item = "[ ] $text "
+					$item = "  $text  "
 				}
 				if($i -eq $menuPosition){
-					Write-Host $item @highLight -NoNewline
+					Write-Host ">$item<" -ForegroundColor Green -NoNewline
 				}else{
-					Write-Host $item -NoNewline
+					Write-Host " $item " -NoNewline
 				}
 			}
 			if(-not (($i +1) % $columnCount)){
@@ -27,8 +27,8 @@ function DrawMenu{
 		}
 	}else{
 		for($i = 0; $i -le $menuItems.length;$i++){
-			if($null -ne $menuItems[$i]){
-				$item = $menuItems[$i]
+			$item = $menuItems[$i]
+			if($item){
 				if($Multiselect)
 				{
 					if($selection -contains $i){
@@ -77,32 +77,29 @@ function Menu{
 				toggle = 32		# Space
 			}
 			if($Table){
-				$columnCount = [Math]::Floor([System.Console]::WindowWidth / (($menuItems | Measure-Object -Property Length -Maximum).Maximum +4))
-				$lineCount = [Math]::Ceiling($menuItems.Count / $columnCount)
+				try{
+					$columnCount = [Math]::Floor([System.Console]::WindowWidth / (($menuItems | Measure-Object -Property Length -Maximum -ErrorAction Stop).Maximum +6))
+					$lineCount = [Math]::Ceiling($menuItems.Count / $columnCount)
+				}catch{ # If the array objects cannot be measured.
+					$columnCount = 0
+					$lineCount = 0
+				}
 			}
 			$startPos = [System.Console]::CursorTop
 			[System.Console]::CursorVisible = $false #prevents cursor flickering
 			DrawMenu $menuItems $pos $Multiselect $selection $columnCount
 			While($keycode -ne 13 -and $keycode -ne 27){
 				$keycode = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown").VirtualKeyCode
-				if($columnCount){
-					switch($keycode){
-						{$keys.up -contains $_ -and $lineCount -gt 1}{$pos = $pos - $columnCount}
-						{$keys.down -contains $_ -and $lineCount -gt 1}{$pos = $pos + $columnCount}
-						{$keys.left -contains $_}{$pos--}
-						{$keys.right -contains $_}{$pos++}
-						{$keys.home -contains $_}{$pos = 0}
-						{$keys.end -contains $_}{$pos = $menuItems.length - 1}
-						{$keys.toggle -contains $_}{$selection = Switch-Selection $pos $selection}
-					}
-				}else{
-					switch($keycode){
-						{$keys.up -contains $_}{$pos--}
-						{$keys.down -contains $_}{$pos++}
-						{$keys.home -contains $_}{$pos = 0}
-						{$keys.end -contains $_}{$pos = $menuItems.length - 1}
-						{$keys.toggle -contains $_}{$selection = Switch-Selection $pos $selection}
-					}
+				switch($keycode){
+					{$keys.up -contains $_ -and $lineCount -gt 1}{$pos = $pos - $columnCount}
+					{$keys.down -contains $_ -and $lineCount -gt 1}{$pos = $pos + $columnCount}
+					{$keys.up -contains $_ -and -not $columnCount}{$pos--}
+					{$keys.down -contains $_ -and -not $columnCount}{$pos++}
+					{$keys.left -contains $_}{$pos--}
+					{$keys.right -contains $_}{$pos++}
+					{$keys.home -contains $_}{$pos = 0}
+					{$keys.end -contains $_}{$pos = $menuItems.length - 1}
+					{$keys.toggle -contains $_}{$selection = Switch-Selection $pos $selection}
 				}
 				switch($pos){ # When attempting to move the cursor outside of the array
 					{$_ -lt 0}{$pos = 0}
@@ -142,5 +139,53 @@ function Menu{
 			return $menuItems[$selection]
 		}
 	}
+
+		
+	<#
+	.SYNOPSIS
+		Displays the menu items, returning the chosen items to the user.
+
+	.DESCRIPTION
+		Displays the given menu items, allowing the user to choose the
+		outputted item(s) by navigating the menu using the arrow keys and
+		validating their selection with the "Enter" key.
+		The "Esc" key will close the menu, returning nothing.
+
+	.PARAMETER menuItems
+		Array containing the items that are to be shown.
+
+	.PARAMETER Multiselect
+		Will allow multiple items to be selected from the displayed menu.
+		Adding an items to the selection is done using the space-bar.
+
+	.PARAMETER ReturnIndex
+		Will return the index of the selected item(s) instead of the item
+		itself.
+	
+	.PARAMETER Table
+		Will display the given items in columns, if the space of the current
+		host window allows for it.
+
+	.EXAMPLE
+		menu @("option 1", "option 2", "option 3")
+
+	.EXAMPLE
+		menu (gci) -MultiSelect
+
+	.EXAMPLE
+		menu (Get-LocalUser).Name -MultiSelect -Table -ReturnIndex
+
+	.INPUTS
+		None. You cannot pipe data into "menu".
+
+	.OUTPUTS
+		The selected menu item(s) or their index.
+
+	.LINK
+		[System.Console]::SetCursorPosition
+
+	.LINK
+		[System.Console]::CursorTop
+	#>
 }
 
